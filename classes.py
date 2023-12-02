4c4bfbad110032c5826ca75150f2589f9a8142bf
ochioci <63116595+ochioci@users.noreply.github.com>
2023-12-02 16:17:10 -0500

    add rook,bishop,queen moves, make gui interactive

--- classes.py
+++ classes.py
@@ -1,30 +1,37 @@
 print("imported classes")
-w=8
-h=8
+w = 8
+h = 8
+
+
 class Piece:
-    def __init__(self,color="empty",square=None):
-        self.dir = 1 if color=="w" else -1
+    def __init__(self, color="empty", square=None):
+        self.dir = 1 if color == "w" else -1
         self.char = ""
         self.square = square
-        self.hasMoved=False
-        self.color=color
-        self.type="empty"
-        self.val=0
+        self.hasMoved = False
+        self.color = color
+        self.type = "empty"
+        self.val = 0
+
     def __str__(self):
-        return self.type[0:1]+self.color[0:1]
+        return self.type[0:1] + self.color[0:1]
+
     def isEmpty(self):
         return self.type == "empty"
+
     def legalMoves(self):
         return []
+    def setSquare(self,sq):
+        self.square=sq
 
-
-class Pawn(Piece): 
-    def __init__(self,color,square=None):
-        super().__init__(color,square)
+class Pawn(Piece):
+    def __init__(self, color, square=None):
+        super().__init__(color, square)
         self.val = 1
         self.char = "o"
-        self.type="pawn"
-    def legalMoves(self):#working
+        self.type = "pawn"
+
+    def legalMoves(self):  # working
         moves = []
         ahead = self.square.board.squareStatus(self.square.x, self.square.y + self.dir, self.color)
         ahead2 = self.square.board.squareStatus(self.square.x, self.square.y + self.dir + self.dir, self.color)
@@ -32,127 +39,284 @@ class Pawn(Piece):
         diagLeft = self.square.board.squareStatus(self.square.x + 1, self.square.y + self.dir, self.color)
         diagRight = self.square.board.squareStatus(self.square.x - 1, self.square.y + self.dir, self.color)
         # print(diagLeft, diagRight)
-        if ahead == 0: #empty
-            moves.append((self.square.x, self.square.y+self.dir))
+        if ahead == 0:  # empty
+            moves.append((self.square.x, self.square.y + self.dir))
             if (not self.hasMoved and ahead2 != 1):
                 moves.append((self.square.x, self.square.y + (2 * self.dir)))
-        if diagRight == 2: #enemy piece
-            moves.append((self.square.x-1, self.square.y + self.dir))
-        if diagLeft == 2: #enemy piece
-            moves.append((self.square.x+1, self.square.y + self.dir))
-        moves = filter((lambda move : not (move[0] >= w or move[1] >= h or move[0] < 0 or move[1] < 0)),moves)
+        if diagRight == 2:  # enemy piece
+            moves.append((self.square.x - 1, self.square.y + self.dir))
+        if diagLeft == 2:  # enemy piece
+            moves.append((self.square.x + 1, self.square.y + self.dir))
+        moves = filter((lambda move: not (move[0] >= w or move[1] >= h or move[0] < 0 or move[1] < 0)), moves)
         return list(moves)
+
+
 class Knight(Piece):
     def legalMoves(self):
-        moves = filter(lambda move: self.square.board.squareStatus(move[0],move[1],self.color) != 1, self.square.getMoves())
+        moves = filter(lambda move: self.square.board.squareStatus(move[0], move[1], self.color) != 1,
+                       self.square.getMoves())
         return list(moves)
-    def __init__(self,color,square=None):
-        super().__init__(color,square)
+
+    def __init__(self, color, square=None):
+        super().__init__(color, square)
         self.val = 3
         self.char = "j"
-        self.type="knight"
+        self.type = "knight"
+
+
 class Rook(Piece):
-    def __init__(self,color,square=None):
-        super().__init__(color,square)
+    def legalMoves(self):
+        moves = []
+        lx,ly = self.square.x, self.square.y
+        for i in range(0, ly)[::-1]:
+            st = self.square.board.squareStatus(lx, i,self.color)
+            if st != 1:
+                moves.append((lx,i))
+            if st != 0:
+                break
+        for i in range(ly+1, 8):
+            st = self.square.board.squareStatus(lx, i,self.color)
+            if st != 1:
+                moves.append((lx,i))
+            if st != 0:
+                break
+        for i in range(0, lx)[::-1]:
+            st = self.square.board.squareStatus(i, ly,self.color)
+            if st != 1:
+                moves.append((i,ly))
+            if st != 0:
+                break
+        for i in range(lx+1, 8):
+            st = self.square.board.squareStatus(i, ly, self.color)
+            if st != 1:
+                moves.append((i,ly))
+            if st != 0:
+                break
+
+        return moves
+    def __init__(self, color, square=None):
+        super().__init__(color, square)
         self.val = 5
-        self.char="t"
-        self.type="rook"
+        self.char = "t"
+        self.type = "rook"
+
 
 class Bishop(Piece):
-    def __init__(self,color,square=None):
-        super().__init__(color,square)
+    def legalMoves(self):#1 is friendly 2 is enemy
+        moves = []
+        lx,ly = self.square.x, self.square.y
+        for a in range(1,8-max(lx,ly)):
+            st = self.square.board.squareStatus(lx+a, ly+a)
+            if st != 1:
+                moves.append(((lx+a),(ly+a)))
+            if st != 0:
+                break
+        for a in range(1,8-max(7-lx,ly)):
+            st = self.square.board.squareStatus(lx-a, ly+a)
+            if st != 1:
+                moves.append(((lx-a),(ly+a)))
+            if st != 0:
+                break
+
+        for a in range(1, min(lx,ly)+1):
+            st = self.square.board.squareStatus(lx-a, ly-a)
+            if st != 1:
+                moves.append(((lx-a),(ly-a)))
+            if st != 0:
+                break
+
+        for a in range(1, min(7-lx,ly)+1):
+            st = self.square.board.squareStatus(lx+a, ly-a)
+            if st != 1:
+                moves.append(((lx+a),(ly-a)))
+            if st != 0:
+                break
+        for i in range(0, ly)[::-1]:
+            st = self.square.board.squareStatus(lx, i, self.color)
+            if st != 1:
+                moves.append((lx, i))
+            if st != 0:
+                break
+        for i in range(ly + 1, 8):
+            st = self.square.board.squareStatus(lx, i, self.color)
+            if st != 1:
+                moves.append((lx, i))
+            if st != 0:
+                break
+        for i in range(0, lx)[::-1]:
+            st = self.square.board.squareStatus(i, ly, self.color)
+            if st != 1:
+                moves.append((i, ly))
+            if st != 0:
+                break
+        for i in range(lx + 1, 8):
+            st = self.square.board.squareStatus(i, ly, self.color)
+            if st != 1:
+                moves.append((i, ly))
+            if st != 0:
+                break
+        return moves
+
+
+
+    def __init__(self, color, square=None):
+        super().__init__(color, square)
         self.val = 3
         self.char = "n"
-        self.type="bishop"
+        self.type = "bishop"
+
+
 class Queen(Piece):
-    def __init__(self,color,square=None):
-        super().__init__(color,square)
+    def legalMoves(self):
+        moves = []
+        lx, ly = self.square.x, self.square.y
+        for a in range(1, 8 - max(lx, ly)):
+            st = self.square.board.squareStatus(lx + a, ly + a)
+            if st != 1:
+                moves.append(((lx + a), (ly + a)))
+            if st != 0:
+                break
+        for a in range(1, 8 - max(7 - lx, ly)):
+            st = self.square.board.squareStatus(lx - a, ly + a)
+            if st != 1:
+                moves.append(((lx - a), (ly + a)))
+            if st != 0:
+                break
+
+        for a in range(1, min(lx, ly) + 1):
+            st = self.square.board.squareStatus(lx - a, ly - a)
+            if st != 1:
+                moves.append(((lx - a), (ly - a)))
+            if st != 0:
+                break
+
+        for a in range(1, min(7 - lx, ly) + 1):
+            st = self.square.board.squareStatus(lx + a, ly - a)
+            if st != 1:
+                moves.append(((lx + a), (ly - a)))
+            if st != 0:
+                break
+        # print(lx, ly, moves)
+        return moves
+
+    def __init__(self, color, square=None):
+        super().__init__(color, square)
         self.val = 9
         self.char = "w"
-        self.type="queen"
+        self.type = "queen"
+
+
 class King(Piece):
-    def __init__(self,color,square=None):
-        super().__init__(color,square)
+    def __init__(self, color, square=None):
+        super().__init__(color, square)
         self.val = 3
         self.char = "l"
-        self.type="king"
+        self.type = "king"
 
 
 class Square:
+    def isEmpty(self):
+        return self.piece.type == "empty"
     def getMoves(self):
         moves = []
         pieceType = self.piece.type
-        match pieceType: #working i think?
-            case "pawn": #missing en passant
-                moves.append((self.x,self.y+(1 if self.piece.color=="w" else -1)))
-                if self.piece.hasMoved == False: moves.append((self.x,self.y+(2 if self.piece.color=="w" else -2)))
-            case "king": moves = [(self.x+i,self.y+n) for i in range(-1,2) for n in range(-1,2)]
-            case "bishop": moves = [(self.x+i,self.y+i) for i in range(-7,8)] + [(self.x-i,self.y+i) for i in range(-7,8)]
-            case "knight": moves = [(self.x + i+i, self.y + i) for i in range(-1,2)] + [(self.x+i, self.y + i +i) for i in range(-1,2)] + [(self.x+i+i,self.y-i) for i in range(-1,2)] + [(self.x+i,self.y-i-i) for i in range(-1,2)]
-            case "rook": moves = [(self.x+i,self.y) for i in range(-7,8)] + [(self.x,self.y+i) for i in range(-7,8)]
-            case "queen": moves =  [(self.x+i,self.y+i) for i in range(-7,8)] + [(self.x-i,self.y+i) for i in range(-7,8)] + [(self.x+i,self.y) for i in range(-7,8)] + [(self.x,self.y+i) for i in range(-7,8)]
-            case default: pass
-        moves = filter((lambda move : not (move[0] >= w or move[1] >= h or move[0] < 0 or move[1] < 0)),moves)
+        match pieceType:  # working i think?
+            case "pawn":  # missing en passant
+                moves.append((self.x, self.y + (1 if self.piece.color == "w" else -1)))
+                if self.piece.hasMoved == False: moves.append((self.x, self.y + (2 if self.piece.color == "w" else -2)))
+            case "king":
+                moves = [(self.x + i, self.y + n) for i in range(-1, 2) for n in range(-1, 2)]
+            case "bishop":
+                moves = [(self.x + i, self.y + i) for i in range(-7, 8)] + [(self.x - i, self.y + i) for i in range(-7, 8)]
+            case "knight":
+                moves = [(self.x + i + i, self.y + i) for i in range(-1, 2)] + [(self.x + i, self.y + i + i) for i in range(-1, 2)] + [(self.x + i + i, self.y - i) for i in range(-1, 2)] + [(self.x + i, self.y - i - i) for i in range(-1, 2)]
+            case "rook":
+                moves = [(self.x + i, self.y) for i in range(-7, 8)] + [(self.x, self.y + i) for i in range(-7, 8)]
+            case "queen":
+                moves = [(self.x + i, self.y + i) for i in range(-7, 8)] + [(self.x - i, self.y + i) for i in range(-7, 8)] + [(self.x + i, self.y) for i in range(-7, 8)] + [(self.x, self.y + i) for i in range(-7, 8)]
+            case default:
+                pass
+        moves = filter((lambda move: not (move[0] >= w or move[1] >= h or move[0] < 0 or move[1] < 0)), moves)
         # print(list(moves))
         return list(moves)
-        
+
     def color(self):
-        return ("forestgreen" if self.clr == 1 else "tan")
-    def setPiece(self,piece):
+        return "forestgreen" if self.clr == 1 else "tan"
+
+    def setPiece(self, piece):
         self.piece = piece
-    def __init__(self,board,x,y,color,piece):
-        self.board=board
+
+    def __init__(self, board, x, y, color, piece):
+        self.board = board
         self.clr = color
-        self.x=x
-        self.y=y
-        self.piece=piece
+        self.x = x
+        self.y = y
+        self.piece = piece
+
     def __str__(self):
-        return ((self.piece.type)[0:1] + self.piece.color[0:1] if self.piece.type != "empty" else self.color()[0:2])
+        return (self.piece.type)[0:1] + self.piece.color[0:1] if self.piece.type != "empty" else self.color()[0:2]
+
+
 class Board:
-    def movePiece(self,x1,y1,x2,y2):
+    def movePiece(self, x1, y1, x2, y2):
         temp = self.board[x1][y1].piece
+        temp.square = self.board[x2][y2]
         self.board[x1][y1].piece = Piece(square=self.board[x1][y1])
         self.board[x2][y2].piece = temp
-    def squareStatus(self,x,y,clr="w"):
+
+
+    def squareStatus(self, x, y, clr="w"):
         # print(x,y, self.board[x][y].piece.color) 
         # print()
-        if self.board[x][y].piece.color == clr: return 1 #friendly piece
-        elif self.board[x][y].piece.color == "empty": return 0
-        else: return 2
+        if self.board[x][y].piece.color == clr:
+            return 1  # friendly piece
+        elif self.board[x][y].piece.color == "empty":
+            return 0
+        else:
+            return 2 # enemy piece
+
     def getEval(self):
         z = self.getSquareWeights()
         return round(sum([sum(z[i]) for i in range(0, len(z))]), 3)
-    def getSquareWeights(self,wClr="w"):
-        return [[round((((h) - ((((i - 3.5) ** 2) + ((n - 3.5) ** 2)) ** 0.33)) / 6) * (1 if self.board[n][i].piece.color == wClr else  -1) * self.board[n][i].piece.val ,3)   for i in range(0, 8)] for n in range(0, 8)]
+
+    def getSquareWeights(self, wClr="w"):
+        return [[round((((h) - ((((i - 3.5) ** 2) + ((n - 3.5) ** 2)) ** 0.33)) / 6) * (
+            1 if self.board[n][i].piece.color == wClr else -1) * self.board[n][i].piece.val, 3) for i in range(0, 8)]
+                for n in range(0, 8)]
+
     def eval(self):
         pass
-    def __init__(self,w=8,h=8):
-        self.board=[]
-        self.w=w
-        self.h=h
-        class Codes: wPieces,bPieces,wPawn,bPawn,lRook,rRook,lKnight,rKnight,lBishop,rBishop,qu,ki=0,h-1,1,h-2,0,w-1,1,w-2,2,w-3,3,4
-        for x in range(0,w):
+
+    def __init__(self, w=8, h=8, buildPawns=True):
+        self.board = []
+        self.w = w
+        self.h = h
+
+        class Codes:
+            wPieces, bPieces, wPawn, bPawn, lRook, rRook, lKnight, rKnight, lBishop, rBishop, qu, ki = 0, h - 1, 1, h - 2, 0, w - 1, 1, w - 2, 2, w - 3, 3, 4
+
+        for x in range(0, w):
             col = []
-            for y in range(0,h):
-                clr = ((x+y)%2)+1
-                piece = Piece(color="empty") 
-                sq = Square(self,x,y,clr,piece)
+            for y in range(0, h):
+                clr = ((x + y) % 2) + 1
+                piece = Piece(color="empty")
+                sq = Square(self, x, y, clr, piece)
                 match y:
-                    case Codes.wPawn|Codes.bPawn:
-                        piece=Pawn("w" if y==Codes.wPawn else "b",sq)
+                    case Codes.wPawn | Codes.bPawn:
+                        if buildPawns: piece = Pawn("w" if y == Codes.wPawn else "b", sq)
                         pass
-                    case Codes.wPieces|Codes.bPieces:
+                    case Codes.wPieces | Codes.bPieces:
                         match x:
-                            case Codes.lRook|Codes.rRook:
-                                piece=Rook("w" if y ==Codes.wPieces else "b",sq)
-                            case Codes.lKnight|Codes.rKnight:
-                                piece=Knight("w" if y == Codes.wPieces else "b",sq)
-                            case Codes.lBishop|Codes.rBishop:
-                                piece=Bishop("w" if y == Codes.wPieces else "b",sq)
+                            case Codes.lRook | Codes.rRook:
+                                piece = Rook("w" if y == Codes.wPieces else "b", sq)
+                            case Codes.lKnight | Codes.rKnight:
+                                piece = Knight("w" if y == Codes.wPieces else "b", sq)
+                            case Codes.lBishop | Codes.rBishop:
+                                piece = Bishop("w" if y == Codes.wPieces else "b", sq)
                             case Codes.qu:
-                                piece=Queen("w" if y == Codes.wPieces else "b",sq)
+                                piece = Queen("w" if y == Codes.wPieces else "b", sq)
                             case Codes.ki:
-                                piece=King("w" if y == Codes.wPieces else "b",sq)
+                                piece = King("w" if y == Codes.wPieces else "b", sq)
                             case default:
                                 pass
                     case default:
@@ -160,33 +324,35 @@ class Board:
                 sq.setPiece(piece)
                 col.append(sq)
             self.board.append(col)
+
     def __str__(self):
         allStr = ""
-        datStr=""
-        for col in range(0,len(self.board[0]))[::-1]:
-            rowStr =""
-            rowStr2=""
-            for row in range(0,len(self.board)):
+        datStr = ""
+        for col in range(0, len(self.board[0]))[::-1]:
+            rowStr = ""
+            rowStr2 = ""
+            for row in range(0, len(self.board)):
                 # print(self.board[row][col].piece.isMoveLegal(0,0))
                 rowStr += str(self.board[row][col])
-                rowStr2+=str(row)+str(col)
+                rowStr2 += str(row) + str(col)
                 # print(self.board[row][col].getMoves())
             allStr += rowStr + "\n"
             datStr += rowStr2 + "\n"
         return allStr + "\n" + "\n" + datStr
+
     def displayRowMajor(self):
-        out="each square is displayed as i,n representing board[i][n]\n"
-        for i in range(0,len(self.board)):
-            r=""
-            for n in range(0,len(self.board[i])):
-                r+=str(i)+str(n)
-            out+=r+'\n'
-        out+="\n\n"
+        out = "each square is displayed as i,n representing board[i][n]\n"
+        for i in range(0, len(self.board)):
+            r = ""
+            for n in range(0, len(self.board[i])):
+                r += str(i) + str(n)
+            out += r + '\n'
+        out += "\n\n"
         for i in self.board:
-            r=""
+            r = ""
             for n in i:
-                r+=str(n)
-            out+=r+'\n'
+                r += str(n)
+            out += r + '\n'
         return out
 
 # myBoard = Board()
--- gui.py
+++ gui.py
@@ -14,12 +14,25 @@ class Window:
         showLoc = True
         font = pygame.font.Font('freesansbold.ttf', 15) if showDebug else pygame.font.Font('chess.ttf', 20)
         font2 = pygame.font.Font('freesansbold.ttf', 40)
+        clicked = (-1, 0)
+        lastClicked = (-1,0)
         while running:
             # poll for events
             # pygame.QUIT event means the user clicked X to close your window
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    xc,yc = pygame.mouse.get_pos()
+                    xc = xc // squareSize
+                    yc = 7-(yc // squareSize)
+                    lastClicked = clicked
+                    clicked = (xc, yc)
+                    # print(self.board[xc][yc].isEmpty())
+                    if lastClicked == clicked:
+                        clicked, lastClicked = (-1,0),(-1,0)
+
+
             # fill the screen with a color to wipe away anything from last frame
             screen.fill("purple")
             self.pieceToTrack = pieceToTrack
@@ -30,41 +43,46 @@ class Window:
                     # print((row,7-col))
                     txtColor = "white" if self.board[col][row].piece.color == "w" else ("black" if self.board[col][row].piece.color == "b" else "blue")
                     # print(txtColor)
-                    if (col,row) == self.pieceToTrack: txtColor = "red"
+                    if (col,row) == clicked and not self.board[col][row].isEmpty(): txtColor = "red"
+                    if (col, row) == lastClicked and not self.board[col][row].isEmpty(): txtColor = "pink"
                     pygame.draw.rect(screen,self.board[col][row].color(), pygame.Rect(col*self.squareSize,(7-row)*self.squareSize,self.squareSize,self.squareSize))
                     text = font.render((str(col)+str(row)+str(self.board[col][row].piece)) if showLoc else str(weights[col][row]) if showDebug else self.board[col][row].piece.char, True, txtColor)
                     textRect = text.get_rect()
                     textRect.center = (((col) +0.5)* self.squareSize, (7-row+0.5) * self.squareSize)
                     screen.blit(text,textRect)
                     
-                        
-            moves = self.board[self.pieceToTrack[0]][self.pieceToTrack[1]].piece.legalMoves()
-  
-            for move in moves:
-                txtColor = "Orange"
-                if (move[0],move[1]) == self.pieceToTrack: txtColor = "red"
-                # print(self.board[move[0]][move[1]].piece.char)
-                txtToWrite = (str(move[0])+str(move[1])+str(self.board[move[0]][move[1]].piece)) if showDebug else (self.board[move[0]][move[1]].piece.char if len(self.board[move[0]][move[1]].piece.char) > 0 else "*")
-                text = font.render(txtToWrite , True, txtColor) if txtToWrite != "*" else font2.render(txtToWrite, True, "Blue")
-                # print(txtToWrite)
-                textRect = text.get_rect()
-                textRect.center = (((move[0]) +0.5)* self.squareSize, (7-move[1]+0.5) * self.squareSize)
-                screen.blit(text,textRect)
 
-            
+            if clicked[0] > -1:
+                moves = self.board[clicked[0]][clicked[1]].piece.legalMoves()
+                for move in moves:
+                    txtColor = "Orange"
+                    if (move[0],move[1]) == self.pieceToTrack: txtColor = "red"
+                    # print(self.board[move[0]][move[1]].piece.char)
+                    txtToWrite = (str(move[0])+str(move[1])+str(self.board[move[0]][move[1]].piece)) if showDebug else (self.board[move[0]][move[1]].piece.char if len(self.board[move[0]][move[1]].piece.char) > 0 else "*")
+                    text = font.render(txtToWrite , True, txtColor) if txtToWrite != "*" else font2.render(txtToWrite, True, "Blue")
+                    # print(txtToWrite)
+                    textRect = text.get_rect()
+                    textRect.center = (((move[0]) +0.5)* self.squareSize, (7-move[1]+0.5) * self.squareSize)
+                    screen.blit(text,textRect)
+
+            # if clicked[0] > -1:
+            #     pygame.draw.rect(screen, "pink", pygame.Rect(clicked[0] * self.squareSize, (7 - clicked[1]) * self.squareSize, self.squareSize, self.squareSize))
+            # if lastClicked[0] > -1:
+            #     pygame.draw.rect(screen, "red", pygame.Rect(lastClicked[0] * self.squareSize, (7 - lastClicked[1]) * self.squareSize, self.squareSize, self.squareSize))
+
             # flip() the display to put your work on screen
             pygame.display.flip()
-            clock.tick(5)  # for every second, at most n frames will pass
+            clock.tick(60)  # for every second, at most n frames will pass
         pygame.quit()
     def quit(self):
         pygame.quit()
-myBoard = Board()
-x1,y1=1,0
+myBoard = Board(buildPawns=False)
+x1,y1=3,3
 # print("moves:")
 # print(myBoard.board[x1][y1].getMoves())
 # print("legal moves")
-myBoard.movePiece(0,6,0,2)
-myBoard.movePiece(2,6,2,2)
+myBoard.movePiece(0,0,3,3)
+# myBoard.movePiece(2,6,2,2)
 a = myBoard.board[x1][y1].piece.legalMoves()
 print("legal moves")
 print(a)
