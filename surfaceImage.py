e329b845dc5ab65ba066698a465cfbcbaf833b59
Ry16 <100397138+Ry160v0@users.noreply.github.com>
2023-12-04 13:56:48 +0800

    Update and rename surfaceImage to surfaceImage.py

--- surfaceImage.py
+++ surfaceImage.py
@@ -23,3 +23,8 @@ surfaceImage = True
 # insert this into the middle of the while loop 
 if surfaceImage:
     drawInterface(screen, self.squareSize, self.board)
+    if clicked[0] > -1:
+    moves = self.board[clicked[0]][clicked[1]].piece.legalMoves()
+    for move in moves:
+        color = ("Red" if len(self.board[move[0]][move[1]].piece.char) > 0 else "Blue")
+        pygame.draw.rect(screen,color,((move[0] * self.squareSize, (7 - move[1]) * self.squareSize),(self.squareSize+1,self.squareSize+1)),width=2)
